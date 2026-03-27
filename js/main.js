5c8df716652ad875162f3df72b4450f4ae07abf0
Tao <sigmundtao@gmail.com>
2026-03-27 21:43:53 +0000

    Add settings menu & theme select

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import { initSearch } from './search.js'
 import { initShortcuts } from './shortcuts.js'
 import { initNavBar } from './navbar.js'
 import { createDefaultTab } from './tabs.js'
+import { initSettings } from './settings.js'
 
 
 initSearch()
 initShortcuts()
 initNavBar()
+initSettings()
 renderFiletree()
 createDefaultTab()
 renderPinnedFiles()
---
+++ js/settings.js
@@ -0,0 +1,28 @@
+const settingEl = document.getElementById('settings')
+const closeSettingsBtn = document.getElementById('close-settings-btn')
+const themeSelectEl = document.getElementById('theme-select')
+const openSettingsBtn = document.getElementById('settings-btn')
+const savedTheme = localStorage.getItem('theme') || 'sakura'
+
+function openSettingsMenu(){
+    console.log('stuff is happening')
+    settingEl.showModal()
+}
+
+function closeSettingsMenu(){
+    settingEl.close()
+}
+
+function setTheme(theme){
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+}
+
+closeSettingsBtn.addEventListener('click', closeSettingsMenu)
+
+export function initSettings(){
+    setTheme(savedTheme)
+    openSettingsBtn.addEventListener('click', openSettingsMenu)
+}
+
+themeSelectEl.addEventListener('change', () => setTheme(themeSelectEl.value))
